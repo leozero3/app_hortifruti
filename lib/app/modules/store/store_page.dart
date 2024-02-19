5539f7b039453cca3384a9d6a4c17e0a5c699826
Leo Home <realleo2009@hotmail.com>
2024-02-19 17:06:00 -0300

    #68

--- lib/app/modules/store/store_page.dart
+++ lib/app/modules/store/store_page.dart
@@ -1,6 +1,7 @@
 import 'package:app_hortifruti/app/widgets/store_status.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:transparent_image/transparent_image.dart';
 import './store_controller.dart';
 
@@ -16,12 +17,13 @@ class StorePage extends GetView<StoreController> {
             const SliverAppBar(),
             SliverToBoxAdapter(
               child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                padding: const EdgeInsets.only(
+                    left: 16, top: 8, right: 16, bottom: 16),
                 child: Row(
                   children: [
                     SizedBox(
                       width: 96,
+                      height: 96,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(8),
                         child: FadeInImage.memoryNetwork(
@@ -48,6 +50,59 @@ class StorePage extends GetView<StoreController> {
                 ),
               ),
             ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  var category = state.categories[index];
+                  return Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                              child: Container(
+                            color: Colors.grey[200],
+                            padding: EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 16),
+                            child: Text(
+                              category.name,
+                              style: Get.textTheme.titleMedium!
+                                  .copyWith(fontWeight: FontWeight.bold),
+                            ),
+                          ))
+                        ],
+                      ),
+                      for (var product in category.products)
+                        ListTile(
+                          title: Text(product.name),
+                          subtitle: Text(
+                            NumberFormat.simpleCurrency().format(product.price),
+                          ),
+                          leading: product.image.isNotEmpty
+                              ? SizedBox(
+                                  width: 56,
+                                  height: 56,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: FadeInImage.memoryNetwork(
+                                      imageErrorBuilder:
+                                          (context, error, stackTrace) =>
+                                              Container(
+                                        width: 56,
+                                      ),
+                                      placeholder: kTransparentImage,
+                                      image: product.image,
+                                    ),
+                                  ),
+                                )
+                              : null,
+                          onTap: () {},
+                        ),
+                    ],
+                  );
+                },
+                childCount: state.categories.length,
+              ),
+            )
           ],
         ),
       ),
