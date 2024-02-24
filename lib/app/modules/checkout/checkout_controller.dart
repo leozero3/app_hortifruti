67c6aeced9877e8c0381c3c0362b916b914b1491
Leo Home <realleo2009@hotmail.com>
2024-02-24 18:39:18 -0300

    #87

--- lib/app/modules/checkout/checkout_controller.dart
+++ lib/app/modules/checkout/checkout_controller.dart
@@ -1,7 +1,13 @@
+import 'package:app_hortifruti/app/data/services/cart/cart_service.dart';
 import 'package:app_hortifruti/app/modules/checkout/checkout_repository.dart';
 import 'package:get/get.dart';
 
 class CheckoutController extends GetxController {
+  final _cartService = Get.find<CartService>();
   final CheckoutRepository _repository;
   CheckoutController(this._repository);
+
+  num get totalCart => _cartService.total;
+  num get deliveryCost => 0;
+  num get totalOrder => totalCart + deliveryCost;
 }
--- lib/app/modules/checkout/checkout_page.dart
+++ lib/app/modules/checkout/checkout_page.dart
@@ -1,16 +1,79 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './checkout_controller.dart';
 
 class CheckoutPage extends GetView<CheckoutController> {
-    
-    const CheckoutPage({Key? key}) : super(key: key);
+  const CheckoutPage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('CheckoutPage'),),
-            body: Container(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CheckoutPage'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Produtos: ',
+                      style: Get.textTheme.titleMedium,
+                    ),
+                  ),
+                  Text(
+                    NumberFormat.simpleCurrency().format(controller.totalCart),
+                    style: Get.textTheme.titleMedium,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Custo de Entrega: ',
+                      style: Get.textTheme.titleMedium,
+                    ),
+                  ),
+                  Text(
+                    NumberFormat.simpleCurrency()
+                        .format(controller.deliveryCost),
+                    style: Get.textTheme.titleMedium,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Total: ',
+                      style: Get.textTheme.titleLarge,
+                    ),
+                  ),
+                  Text(
+                    NumberFormat.simpleCurrency().format(controller.totalOrder),
+                    style: Get.textTheme.titleLarge,
+                  )
+                ],
+              ),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Enviar pedido'))
+          ],
+        ),
+      ),
+    );
+  }
 }
