e4d4087cd50c8663ae7cb5d6ac8e695971cb815f
Leo Home <realleo2009@hotmail.com>
2024-02-26 22:28:05 -0300

    #92

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -3,14 +3,92 @@ import 'package:flutter/material.dart';
 import './login_controller.dart';
 
 class LoginPage extends GetView<LoginController> {
-    
-    const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('LoginPage'),),
-            body: Container(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LoginPage'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Text(
+              //   'Login',
+              //   style: const TextStyle(
+              //     fontSize: 30,
+              //     fontWeight: FontWeight.bold,
+              //   ),
+              // ),
+              // const SizedBox(height: 20),
+              TextFormField(
+                controller: controller.emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return 'Informe seu Email';
+                  }
+                  if (value != null && !value.contains('@')) {
+                    return 'Informe um Email valido';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: controller.passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return 'Informe a sua Senha';
+                  }
+
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          controller.login();
+                        },
+                        child: const Text('Entrar'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Text('Quero criar uma conta'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
